de8656d9b24957a450c6f50b1cd0c09fbf79d580
Pavel Ivanovski <p@singulart.io>
2021-01-27 21:18:29 +0300

    message on ViewController

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
