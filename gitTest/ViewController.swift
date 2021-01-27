7f919fe6e4c8ed6f8748fb76ed0cbc91e54ed286
Pavel Ivanovski <p@singulart.io>
2021-01-27 21:12:52 +0300

    new hello's

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+        print("hello everybody")
+        print("Hi there")
     }
 
 
