bd1e099e91fc7436280ab28523f449829989e975
Pavel Ivanovski <p@singulart.io>
2021-01-27 20:29:56 +0300

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Pavel Ivanovski on 27.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
